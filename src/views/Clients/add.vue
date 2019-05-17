e3196fcdf9ca9f5ea55c71e5e0e2f1967b905828
笑笑 <xxred0324@qq.com>
2019-05-17 21:59:37 +0800

    客户端相关页面暂存

---
+++ src/views/Clients/add.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-form
+    ref="dataForm"
+    :model="temp"
+    label-position="left"
+    label-width="120px"
+    style="width: 500px; margin-left:50px;"
+  >
+    <el-form-item
+      label="客户端名称"
+      prop="clientName"
+    >
+      <el-input
+        v-model="temp.clientName"
+        type="text"
+      />
+    </el-form-item>
+    <el-form-item
+      label="客户端类型"
+      prop="clientType"
+    >
+      <div style="margin-top: 20px">
+        <el-radio
+          v-model="temp.clientType"
+          label="0"
+          border
+        >空 - 默认</el-radio>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio
+          v-model="temp.clientType"
+          label="1"
+          border
+        >Web 应用程序 - 服务器端 混合流</el-radio>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio
+          v-model="temp.clientType"
+          label="2"
+          border
+        >单页应用程序 - Javascript
+          Authorization Code Flow with PKCE</el-radio>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio
+          v-model="temp.clientType"
+          label="3"
+          border
+        >原生应用程序 - 移动/桌面 混合流</el-radio>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio
+          v-model="temp.clientType"
+          label="4"
+          border
+        >机械/机器人 - 资源所有者密码和客户端凭据流</el-radio>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio
+          v-model="temp.clientType"
+          label="5"
+          border
+        >电视和限制输入设备应用程序 - 设备流程</el-radio>
+      </div>
+
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      temp: {
+        clientName: '',
+        clientType: ''
+      }
+    }
+  }
+}
+</script>
--- src/views/Clients/index.vue
+++ src/views/Clients/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <table-base :table-name="tableName">
+  <table-base
+    :table-name="tableName"
+    ref="tableBase"
+  >
     <template slot="tableColumns">
       <el-table-column
         label="客户端名称"
@@ -12,19 +15,51 @@
     </template>
 
     <template slot="formColumns">
-666
+      <add ref="add"></add>
+    </template>
+
+    <template slot="dialogFooter">
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button
+          type="primary"
+          @click="$refs.tableBasedialogStatus === 'create' ? createData() : updateData()"
+        >确认</el-button>
+      </div>
     </template>
   </table-base>
 </template>
 <script>
 import TableBase from '../../components/tableBase'
+import Add from './add'
+import { createData } from '../../../../Easy.Front-End/src/api/base'
+
 const tableName = 'Clients'
 export default {
   name: tableName,
-  components: { TableBase },
+  components: { TableBase, Add },
   data() {
     return {
       tableName: tableName
+    }
+  },
+  methods: {
+    createData() {
+      createData(this.tableName, this.$refs.add).then(() => {
+        this.$refs.tableBase.dialogFormVisible = false
+        this.$notify({
+          title: '成功',
+          message: '创建成功',
+          type: 'success',
+          duration: 2000
+        })
+      })
+    },
+    updateData(){
+
     }
   }
 }
