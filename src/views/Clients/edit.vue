a1197a5ef6b09da7a7f99f4a5099d50906cb1d6c
笑笑 <xxred0324@qq.com>
2019-05-22 00:22:08 +0800

    客户端添加页完善和编辑页添加新文件

---
+++ src/views/Clients/edit.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-form
+    ref="dataForm"
+    :model="temp"
+    label-position="left"
+    label-width="120px"
+    style="width: 500px; margin-left:50px;"
+  >
+    <el-form-item
+      label="客户端名称"
+      prop="clientName"
+    >
+      <el-input
+        v-model="temp.clientName"
+        type="text"
+      />
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      temp: {
+        clientName: '',
+        clientType: ''
+      }
+    }
+  }
+}
+</script>
--- src/views/Clients/index.vue
+++ src/views/Clients/index.vue
@@ -1,6 +1,8 @@
 <template>
   <table-base
     :table-name="tableName"
+    :afterClickAddBtnFun="afterClickAddBtnFun"
+    :afterClickEditBtnFun="afterClickEditBtnFun"
     ref="tableBase"
   >
     <template slot="tableColumns">
@@ -15,40 +17,63 @@
     </template>
 
     <template slot="formColumns">
-      <add ref="add"></add>
+      <add
+        v-if="dialogStatus === 'create'"
+        ref="add"
+      ></add>
+      <edit
+        v-else-if="dialogStatus === 'update'"
+        ref="edit"
+      ></edit>
     </template>
 
     <template slot="dialogFooter">
-      <div
-        slot="footer"
-        class="dialog-footer"
-      >
-        <el-button @click="dialogFormVisible = false">取消</el-button>
-        <el-button
-          type="primary"
-          @click="$refs.tableBasedialogStatus === 'create' ? createData() : updateData()"
-        >确认</el-button>
-      </div>
+      <el-button @click="$refs.tableBase.dialogFormVisible = false">取消</el-button>
+      <el-button
+        type="primary"
+        @click="$refs.tableBase.dialogStatus === 'create' ? createData() : updateData()"
+      >确认</el-button>
     </template>
   </table-base>
 </template>
 <script>
 import TableBase from '../../components/tableBase'
 import Add from './add'
-import { createData } from '../../../../Easy.Front-End/src/api/base'
+import Edit from './edit'
+import { createData } from '@xxred/easy-front-end/src/api/base'
+import { setTimeout } from 'timers'
 
 const tableName = 'Clients'
 export default {
   name: tableName,
-  components: { TableBase, Add },
+  components: { TableBase, Add, Edit },
   data() {
     return {
-      tableName: tableName
+      tableName: tableName,
+      dialogStatus: 'create'
     }
   },
   methods: {
+    afterClickAddBtnFun() {
+      this.dialogStatus = 'create'
+      this.$nextTick(() => {
+        this.$refs.add.temp = {}
+        this.$refs.add.$refs.dataForm.clearValidate()
+      })
+    },
+    afterClickEditBtnFun(row) {
+      this.dialogStatus = 'update'
+      // 延时赋值，给ui更新一点准备时间
+      setTimeout(() => {
+        this.$refs.edit.temp = Object.assign({}, row)
+      }, 100)
+
+      this.$nextTick(() => {
+        this.$refs.edit.$refs.dataForm.clearValidate()
+      })
+    },
     createData() {
-      createData(this.tableName, this.$refs.add).then(() => {
+      createData(this.tableName, this.$refs.add.temp).then(() => {
         this.$refs.tableBase.dialogFormVisible = false
         this.$notify({
           title: '成功',
@@ -56,11 +81,10 @@ export default {
           type: 'success',
           duration: 2000
         })
+        this.$refs.tableBase.handleFilter()
       })
     },
-    updateData(){
-
-    }
+    updateData() {}
   }
 }
 </script>
